955bf743310af95ae7bad05980c0b4e36f3d84c5
ZiKim <zengkings@foxmail.com>
2018-02-22 23:59:01 +0800

    init

---
+++ src/view/Basewin.java
@@ -0,0 +1,20 @@
+package view;
+
+import xmlpulldemo.AttributeSet;
+import xmlpulldemo.Params;
+
+public class Basewin {
+	
+	public void addView(Basewin win, Params params){
+		
+	}
+	
+	public void setLayoutParams(Params params){
+		
+	}
+	
+	public Params generateLayoutParams(AttributeSet attrs){
+		return null;
+	}
+
+}
